5447c8f52ba20ab765407917eace95b4f4176096
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-13 18:50:23 +1000

    first commit

---
+++ card_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe('math_calculations') do
+  it 'does basic math' do
+    expect(1 + 1).to eq(2)
+    expect(10 - 4).to eq(6)
+    expect(4 * 2).to eq(8)
+    expect(6 / 3).to eq(2)
+  end
+end
